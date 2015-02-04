07075d9727e91ebd4eda46ada7880acb92dcebf9
Florian Zorbach <florian.zorbach@powerserverplus.net>
2015-02-04 20:51:10 +0100

    fixed scaling

--- Source/MVC/View/Components/LiveView.cs
+++ Source/MVC/View/Components/LiveView.cs
@@ -14,10 +14,17 @@ namespace YoloTrack.MVC.View.Components
         protected Image m_image = null;
         protected int m_count = 0;
 
-        public LiveView()
+        public LiveView ()
+		{
+			InitializeComponent ();
+			this.DoubleBuffered = true;
+			this.ImageAlign = ContentAlignment.TopLeft;
+        }
+		
+        public ContentAlignment ImageAlign
         {
-            InitializeComponent();
-            this.DoubleBuffered = true;
+            get;
+            set;
         }
 
         public Image Image
@@ -39,23 +46,129 @@ namespace YoloTrack.MVC.View.Components
             }
         }
 
-        protected override void OnPaint (PaintEventArgs e)
-		{
-			if (this.Image == null) {
-				return;
-			}
-            
-
-			//e.Graphics.DrawImage (this.Image, new Point (0, 0)); /* Old Code */
-			
-			e.Graphics.DrawImage (
-				this.Image, 					/* Image instance */
-				0, 								/* Target X */
-				0, 								/* Target Y */
-				this.ClientRectangle.Width, 	/* Target Width */
-				this.ClientRectangle.Height		/* Target Height */
-			);
-            
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (this.Image == null)
+            {
+                return;
+            }
+ 
+ 
+            float ar = (float)this.Image.Width / (float)this.Image.Height;
+            float nheight, nwidth;
+            int xpos = 0;
+            int ypos = 0;
+ 
+            if ((float)this.ClientRectangle.Height * ar < (float)ClientRectangle.Width)
+            {
+                nheight = (float)this.ClientRectangle.Height;
+                nwidth = (float)this.ClientRectangle.Height * ar;
+            }
+            else
+            {
+                nheight = (float)this.ClientRectangle.Width * 1/ar;
+                nwidth = (float)this.ClientRectangle.Width;
+            }
+ 
+            switch (this.ImageAlign)
+            {
+                #region Alignment Top
+                case ContentAlignment.TopLeft:
+                    break;
+               
+ 
+                case ContentAlignment.TopRight:
+                    if (nwidth < this.ClientRectangle.Width)
+                    {
+                        xpos = this.ClientRectangle.Width - (int)nwidth;
+                    }
+                    break;
+ 
+                case ContentAlignment.TopCenter:
+                    if (nwidth < this.ClientRectangle.Width)
+                    {
+                        xpos = (int)((float)(this.ClientRectangle.Width - (int)nwidth) / 2);
+                    }
+                    break;
+                #endregion
+                #region Alignment Middle
+                case ContentAlignment.MiddleLeft:
+                    if (nheight < this.ClientRectangle.Height)
+                    {
+                        ypos = (int)((float)(this.ClientRectangle.Height - (int)nheight) / 2);
+                    }
+                    break;
+ 
+                case ContentAlignment.MiddleCenter:
+                    if (nwidth < this.ClientRectangle.Width)
+                    {
+                        xpos = (int)((float)(this.ClientRectangle.Width - (int)nwidth) / 2);
+                    }
+ 
+                    if (nheight < this.ClientRectangle.Height)
+                    {
+                        ypos = (int)((float)(this.ClientRectangle.Height - (int)nheight) / 2);
+                    }
+                    break;
+ 
+                case ContentAlignment.MiddleRight:
+ 
+                    if (nwidth < this.ClientRectangle.Width)
+                    {
+                        xpos = this.ClientRectangle.Width - (int)nwidth;
+                    }
+ 
+                    if (nheight < this.ClientRectangle.Height)
+                    {
+                        ypos = (int)((float)(this.ClientRectangle.Height - (int)nheight) / 2);
+                    }
+                    break;
+                #endregion
+                #region Alignment Bottom
+                case ContentAlignment.BottomLeft:
+                    if (nheight < this.ClientRectangle.Height)
+                    {
+                        ypos = this.ClientRectangle.Height - (int)nheight;
+                    }
+                    break;
+ 
+                case ContentAlignment.BottomCenter:
+                    if (nheight < this.ClientRectangle.Height)
+                    {
+                        ypos = this.ClientRectangle.Height - (int)nheight;
+                    }
+                    if (nwidth < this.ClientRectangle.Width)
+                    {
+                        xpos = (int)((float)(this.ClientRectangle.Width - (int)nwidth) / 2);
+                    }
+                    break;
+ 
+                case ContentAlignment.BottomRight:
+                    if (nheight < this.ClientRectangle.Height)
+                    {
+                        ypos = this.ClientRectangle.Height - (int)nheight;
+                    }
+                    if (nwidth < this.ClientRectangle.Width)
+                    {
+                        xpos = this.ClientRectangle.Width - (int)nwidth;
+                    }
+                    break;
+ 
+                #endregion
+ 
+                default:
+                    throw new NotImplementedException("FAILERRORFAIL");
+            }
+ 
+            e.Graphics.Clear(Color.Purple);
+ 
+            e.Graphics.DrawImage(
+                this.Image, /* Image instance */
+                xpos, /* Target X */
+                ypos, /* Target Y */
+                nwidth, /* Target Width */
+                nheight /* Target Height */
+            );
         }
     }
 }
