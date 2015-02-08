329248f12e71ac49342619718209fa732921526c
florian0 <florian0@project-hazard.org>
2015-02-08 16:41:35 +0100

    some moar documentation

--- Source/MVC/View/Components/VisualTimer.cs
+++ Source/MVC/View/Components/VisualTimer.cs
@@ -6,116 +6,132 @@ using System.Windows.Forms;
 namespace YoloTrack.Source.MVC.View.Components
 {
     /// <summary>
-    /// 
+    /// Beautiful visual timer control
     /// </summary>
     public partial class VisualTimer : UserControl
     {
         /// <summary>
-        /// 
+        /// Fired, when this countdown timeout occurs
         /// </summary>
         public event EventHandler Timeout;
 
         /// <summary>
-        /// 
+        /// Get the current step value
         /// </summary>
         [DefaultValue(0)]
-        public double Step { get; set; }
+        public double Step { get; protected set; }
 
         /// <summary>
-        /// 
+        /// Width of the animated arc
         /// </summary>
         [DefaultValue(10)]
         public int ArcWidth { get; set; }
 
         /// <summary>
-        /// 
+        /// Timeout in seconds
         /// </summary>
         public int TimeoutValue { get; set; }
 
         /// <summary>
-        /// 
+        /// (internal) Stores the starttime
         /// </summary>
         private DateTime m_start;
 
         /// <summary>
-        /// 
+        /// Default constructor
         /// </summary>
         public VisualTimer()
         {
-            InitializeComponent();
-            DoubleBuffered = true;
+            this.InitializeComponent();
+            this.DoubleBuffered = true;
         }
 
         /// <summary>
-        /// 
+        /// Reset and start the Timer
         /// </summary>
         public void Start()
         {
-            timer1.Start();
-            Step = 0;
-            m_start = DateTime.Now;
+            this.m_timer.Start();
+            this.Step = 0;
+            this.m_start = DateTime.Now;
         }
 
         /// <summary>
-        /// 
+        /// Start the timer
         /// </summary>
+        /// <param name="Seconds">Timeoutvalue in seconds</param>
         public void Start(int Seconds)
         {
-            TimeoutValue = Seconds;
-            Start();
+            this.TimeoutValue = Seconds;
+            this.Start();
         }
 
         /// <summary>
-        /// 
+        /// EventHandler for timer-tick (force redraw layout)
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void timer1_Tick(object sender, EventArgs e)
+        private void m_timer_Tick(object sender, EventArgs e)
         {
-            if (TimeoutValue - (DateTime.Now - m_start).TotalSeconds <= 0)
+            /* Check if timout is reached */
+            if (this.TimeoutValue - (DateTime.Now - this.m_start).TotalSeconds <= 0)
             {
-                timer1.Stop();
+                this.m_timer.Stop();
                 return;
             }
+            /* Calculate current step value */
+            this.Step = (DateTime.Now - this.m_start).TotalMilliseconds;
 
-            Step = (DateTime.Now - m_start).TotalMilliseconds;
+            /* Force redrawing the whole control */
+            this.Invalidate();
 
-            Invalidate();
-            label_time.Text = ((int)(TimeoutValue - (DateTime.Now - m_start).TotalSeconds)).ToString();
+            /* Cast to int because Math.Round() is for faggets */
+            this.label_time.Text = ((int)(this.TimeoutValue - (DateTime.Now - this.m_start).TotalSeconds)).ToString();
         }
 
         /// <summary>
-        /// 
+        /// OnPaint Method for drawing the layout
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void VisualTimer_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.FillRectangle(new SolidBrush(BackColor), ClientRectangle);
-            label_time.Font = Font;
-            label_time.ForeColor = ForeColor;
+            /* Clear previous drawn stuff */
+            e.Graphics.Clear(this.BackColor);
+            
+            /* Set Font */
+            this.label_time.Font = this.Font;
+            this.label_time.ForeColor = this.ForeColor;
 
-            if (Step == 0)
+            /* Set the SmoothingMode */
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            /* Skip first step (div / 0 fail) */
+            if (this.Step == 0)
             {
                 return;
             }
 
+            /* Declare paths for the arc */
             System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
             System.Drawing.Drawing2D.GraphicsPath path_back = new System.Drawing.Drawing2D.GraphicsPath();
 
+            /* Draw the inner and the outer border using arc */
             path.AddArc(new Rectangle(1, 1, Width - 2, Height - 2), -90, _calc_angle(Step));
             path_back.AddArc(new Rectangle(1 + ArcWidth, 1 + ArcWidth, Width - 2 - ArcWidth * 2, Height - 2 - ArcWidth * 2), -90, _calc_angle(Step));
             
+            /* Reverse on path for correct drawing */
             path_back.Reverse();
             path.AddPath(path_back, true);
-            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            e.Graphics.FillPath(new SolidBrush(ForeColor), path);
+
+            /* Fill the Path */
+            e.Graphics.FillPath(new SolidBrush(this.ForeColor), path);
             
             e.Dispose();
         }
 
         /// <summary>
-        /// 
+        /// Shorthand-function
         /// </summary>
         /// <param name="a"></param>
         /// <returns></returns>
